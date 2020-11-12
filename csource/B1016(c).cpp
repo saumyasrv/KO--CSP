9711229b2f59abbe90efee88b1dfb809fde90121
Song Chuanqi <2274931208@qq.com>
2020-11-12 11:30:32 +0800

    B1016C解法

--- csource/B1016(c).cpp
+++ csource/B1016(c).cpp
@@ -1,25 +1,25 @@
-#include "stdio.h"
+#include <stdio.h>
 int main()
 {   
-  long a,b,da,db;
-  long pa=0,pb=0;
-  scanf("%ld %ld %ld %ld",&a,&b,&da,&db);
-  while(a!=0)
+  long a , b , da , db;
+  scanf("%ld %ld %ld %ld" , &a , &da , &b , &db);
+  long long pa = 0 , pb = 0;
+  while(a != 0)
   {
-     if(a%10==da)
+     if(a % 10 == da)
      {
-         pa=pa*10+da;
+        pa = pa * 10 + da;
      }
-     a=a/10;
+     a = a / 10;
   }
-   while(b!=0)
+   while( b != 0)
   {
-     if(b%10==db)
+     if(b % 10 == db)
      {
-         pb=pb*10+db;
+        pb = pb * 10 + db;
      }
-     b=b/10;
+     b = b / 10;
   }
-  printf("pa+pb=%ld\n",(pa+pb));
+  printf(" pa + pb = %lld\n",(pa+pb));
   return 0;
 }
