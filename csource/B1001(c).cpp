68cac1c151d42a1f2e970a14b35706eca1b00d95
Song Chuanqi <2274931208@qq.com>
2020-11-22 17:35:16 +0800

    B1001C语言解法

---
+++ csource/B1001(c).cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int step = 0 , n;
+    scanf("%d" , &n);
+    while (n != 1) 
+    {
+        if (n % 2 == 0) 
+        {
+           n = n / 2;
+        } 
+        else 
+        {
+           n = (3 * n + 1) / 2;
+        }
+           step++;
+    }
+    printf("%d\n" , step);
+    return 0;
+}
