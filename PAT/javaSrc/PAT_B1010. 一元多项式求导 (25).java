61df475282c6f7b0151af923d7e194268cb28df2
QuakeWang <45645138+QuakeWang@users.noreply.github.com>
2021-02-05 20:16:46 +0800

    Update PAT_B1010. 一元多项式求导 (25).java
    增加注释

--- PAT/javaSrc/PAT_B1010. 一元多项式求导 (25).java
+++ PAT/javaSrc/PAT_B1010. 一元多项式求导 (25).java
@@ -3,20 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int flag = 0;
+        int flag = 0;// 用于判断是否完成所有数据
+        
+        // 使用Scanner类中的hsaNext() 方法来处理输入
         while (in.hasNext()) {
-            int x = in.nextInt();
-            int n = in.nextInt();
+            int x = in.nextInt();// x为系数
+            int n = in.nextInt();// n为指数
             if (n != 0) {
                 if (flag == 0) {
-                    System.out.print(x * n);
+                    System.out.print(x * n);// 输出求导后的系数
                 } else {
-                    System.out.print(" " + x * n);
+                    System.out.print(" " + x * n);// 加上空格
                 }
-                System.out.print(" " + (n - 1));
-                flag = 1;
+                System.out.print(" " + (n - 1));// 输出求导后的指数
+                flag = 1;// 处理结束
             }
         }
+        
+        // 处理特殊情况
         if (flag == 0) {
             System.out.print("0 0");
         }
