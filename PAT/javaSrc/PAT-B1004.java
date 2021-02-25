e15755a37d7c48d31b960d51319fc18c231fff7f
Song Chianqi <73341081+freedom3219@users.noreply.github.com>
2021-02-25 10:34:56 +0800

    Update PAT-B1004.java

--- PAT/javaSrc/PAT-B1004.java
+++ PAT/javaSrc/PAT-B1004.java
@@ -1,9 +1,7 @@
-package com.company;
-
 import java.util.Scanner;
 
-public class B1004 {
-    //定义student类
+public class Main {
+    // 定义student类
     private static class student {
         String name;
         String ID;
@@ -12,27 +10,27 @@ public class B1004 {
 
     public static void main(String[] args) {
         Scanner scannner = new Scanner(System.in);
-        int N = scannner.nextInt();//输入学生人数
+        int N = scannner.nextInt();// 输入学生人数
         int maxScore = -1;
         int maxID = 0;
         int minScore = 101;
         int minID = 0;
-        student[] B = new student[N];//定义数组B
+        student[] B = new student[N];// 定义数组B
         for (int i = 0; i < N; i++) {
-            B[i] = new student();//引用student类
-            B[i].name = scannner.next();//输入学生姓名
-            B[i].ID = scannner.next();//输入学生考号
-            B[i].score = scannner.nextInt();//输入学生分数
-            if (B[i].score > maxScore) {  //比较出分数较大的学生姓名和考号
+            B[i] = new student();// 引用student类
+            B[i].name = scannner.next();// 输入学生姓名
+            B[i].ID = scannner.next();// 输入学生考号
+            B[i].score = scannner.nextInt();// 输入学生分数
+            if (B[i].score > maxScore) {  // 比较出分数较大的学生姓名和考号
                 maxScore = B[i].score;
                 maxID = i;
             }
-            if (B[i].score < minScore) {  //比较出分数较小的学生姓名和考号
+            if (B[i].score < minScore) {  // 比较出分数较小的学生姓名和考号
                 minScore = B[i].score;
                 minID = i;
             }
         }
-        //输出较大的和较小的人的姓名和考号
+        // 输出较大的和较小的人的姓名和考号
         if (N != 0) {
             System.out.println(B[maxID].name + " " + B[maxID].ID);
             System.out.println(B[minID].name + " " + B[minID].ID);
