8107837e3cdda5fcb68edac2b5fb873cc73ccf59
Song Chuanqi <73341081+freedom3219@users.noreply.github.com>
2021-03-31 20:59:37 +0800

    Update PAT-B1030.完美数列.java

--- PAT/javaSrc/PAT-B1030.完美数列.java
+++ PAT/javaSrc/PAT-B1030.完美数列.java
@@ -6,21 +6,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		double p = sc.nextInt();
-		ArrayList<Integer> list = new ArrayList<Integer>();
+		int N = sc.nextInt(); // 输入整数 N
+		double p = sc.nextInt(); // 输入整数 p
+		ArrayList<Integer> list = new ArrayList<Integer>(); // 定义一个 ArrayList<> 泛型数组
 		for (int i = 0; i < N; i++) {
-			list.add(sc.nextInt());
+			list.add(sc.nextInt()); // 遍历添加 N 个数
 		}
-		sc.close();
-		Collections.sort(list);
-		int count = 0;
-		for (int i = 0; i < N; i++) {
+		sc.close(); // 关闭输入
+		Collections.sort(list); // 对添加的数进行排序
+		int count = 0; // 定义 count 来输出符合条件的个数
+		for (int i = 0; i < N; i++) { // 用双重 for() 循环遍历，找出完美数列的个数
 			for (int j = i+count; j < N; j++) {
-				if(list.get(j) > list.get(i)*p)  break;
-				if( j-i+1 > count) count = j-i+1;
+				if(list.get(j) > list.get(i)*p)  break; // 用 if() 判断出符合条件的数
+				if( j-i+1 > count) count = j-i+1; // 筛选出符合 count 的个数
 			}
 		}
-		System.out.print(count);
+		System.out.print(count); // 输出 count 的个数
 	}
 }
