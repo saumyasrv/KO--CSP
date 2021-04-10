cb49187d2791046f45510d7d1038be0b212d6b5e
Galaxy-Hitchhiker <65391259+Galaxy-Hitchhiker@users.noreply.github.com>
2021-04-11 02:03:49 +0800

    Update PAT-B1036.和奥巴马一起编程.java

--- PAT/javaSrc/PAT-B1036.和奥巴马一起编程.java
+++ PAT/javaSrc/PAT-B1036.和奥巴马一起编程.java
@@ -4,14 +4,15 @@ public class Main {
     public static void main(String[] args) {
        Scanner in = new Scanner(System.in);
        int N = in.nextInt();  // 输入正方形边长 N
-       String c = in.next();
-
+       String c = in.next();  //输入组成正方形边的某种字符 C
        int t = N / 2 + N % 2; // 计算出有多少行
+        
+        //输出第一行 c
         for (int i = 0; i < N; i++){
-            System.out.print(c);
+            System.out.print(c); 
         }
 
-        System.out.print("\n");
+        System.out.print("\n"); //换行
 
         // 输出第 2 ~ t-1 行
         for (int i = 0; i < t - 2; i++)
@@ -22,6 +23,8 @@ public class Main {
                 System.out.print(c);     // 输出最后一个 c
                 System.out.print("\n");  //换行
         }
+        
+        // 输出最后一行 c
         for (int i = 0; i < N; i++){
             System.out.print(c);
         }
