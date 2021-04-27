9e7b2cfe11ee33b3f2aedb66a17e6e0b51deebcc
JaydenHD <65391259+JaydenHD@users.noreply.github.com>
2021-04-27 16:49:43 +0800

    Update and rename PAT-B1016.部分A + B.java to PAT-B1016.部分A+B.java
    delete the blank sapce in the title

--- PAT/javaSrc/PAT-B1016.部分A+B.java
+++ PAT/javaSrc/PAT-B1016.部分A+B.java
@@ -1,25 +1,5 @@
 import java.util.Scanner;
 
-/**
- * B1016：部分A + B
- * 题目描述：
- * 正整数A的 “DA（为1位整数）部分” 定义为由A中所有DA 组成的新整数PA 。例如：给定A = 3862767， D~A = 6，则A的“6的部分”PA 是66，因为A中有2个6。
- * <p>
- * 输入格式：
- * 在一行中依次输入A、DA、B、DB ，中间以空格分隔，其中 A > 0, B < 10^10。
- * <p>
- * 输出格式：
- * 在一行中输出PA + PB 的值。
- * <p>
- * 输入样例1：
- * 3862767 6 13530293 3
- * 输出样例1：
- * 399
- * 输入样例2：
- * 3862767 1 13530293 8
- * 输出样例2：
- * 0
- */
 public class B1016 {
     public static void main(String[] args) {
         B1016Solution b1016Solution = new B1016Solution();
